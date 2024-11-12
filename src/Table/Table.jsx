0185a219d73ad2e8ab155848b25b4a8269e6a134
Elias Chamma <elias.john.chamma@gmail.com>
2024-11-12 13:42:07 -0600

    initial stuffs

---
+++ src/Table/Table.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+const Table = () =>
+{
+    const [items,setItems] = useState([]);
+    const [collections, setCollections] = useState([]);
+    
+    const colView = () =>
+        
+}
